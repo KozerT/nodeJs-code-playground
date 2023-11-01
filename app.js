a13777f84a284a494cde0f1a934aeb73c912015c
KozerT <kozer.vizastaff@gmail.com>
2023-11-01 21:06:00 +0100

    app.js: first simple handling get request created

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const express = require('express');
-// const fs = require('fs');
-// const app = express();
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
 // //3.Define route. Route means- how our app response to a certain url, HTTP method, that's been used to that request
 // //changing send to json, bc send just send to the client
@@ -21,8 +21,12 @@
 // app.listen(port, () => {
 //   console.log(`App running on port ${port}...`);
 // });
-/////==================TO get all the tours
+
+/////===============HANDLING GET REQUEST:file-based API;
 //2.Before send the data , we have to read it:
+//---_dirname - folder where is the current script is located;
+//JSON.parse - the data after this, will be automatically converted into JS object;
+
 const tours = JSON.parse(
   fs.readFileSync(`${_dirname}/dev-data/data/tours-simple.json`)
 );
@@ -30,8 +34,9 @@ const tours = JSON.parse(
 //1.to send back to the client  all the tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
+    // we have to specify what we gonna send back;
     status: 'success',
-    results: tours.lengths,
+    results: tours.lengths, //This makes sense, when we are sending an array. :To see # of results that we are sanding;
     data: {
       tours: tours,
     },
